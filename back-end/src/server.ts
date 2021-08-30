20d26bc043ba2f98fa66f51d40b3e9f12eeb64d3
joaogabrielramos28 <joaogabrielramos28@outlook.com>
2021-08-30 20:37:12 -0300

    First commit 🎈

---
+++ back-end/src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+
+
+
+app.get("/",(request,response)=>{
+    response.json({message:"Hello World"})
+})
+
+
+app.listen((3000),()=>{
+    console.log("Application run on port 3000 🧨🎇🎆");
+})
