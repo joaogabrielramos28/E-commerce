ee4b9cd6681c3efe4bb1a9cc6a56c1378a246fd6
joaogabrielramos28 <joaogabrielramos28@outlook.com>
2021-08-30 22:06:09 -0300

    adicionando padroes de projeto

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,14 +1,14 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
+const app = express();
 
 
 
-app.get("/",(request,response)=>{
-    response.json({message:"Hello World"})
-})
+app.get('/',(request,response)=>{
+    response.json({message:'Hello World'});
+});
 
 
 app.listen((3000),()=>{
-    console.log("Application run on port 3000 🧨🎇🎆");
-})
+    console.log('Application run on port 3000 🧨🎇🎆');
+});
