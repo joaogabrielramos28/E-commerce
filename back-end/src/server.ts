0e3ddf0de3d5653cd488fb5dbc0effb9b9d3480d
joaogabrielramos28 <joaogabrielramos28@outlook.com>
2021-08-30 22:21:54 -0300

    mudando padrão de projeto

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -2,13 +2,10 @@ import express from 'express';
 
 const app = express();
 
-
-
-app.get('/',(request,response)=>{
-    response.json({message:'Hello World'});
+app.get('/', (request, response) => {
+  response.json({ message: 'Hello World' });
 });
 
-
-app.listen((3000),()=>{
-    console.log('Application run on port 3000 🧨🎇🎆');
+app.listen((3000), () => {
+  console.log('Application run on port 3000 🧨🎇🎆');
 });
