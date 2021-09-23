f18c6015fab3fb6a6572dcb5cd5e5deb1ab79774
joaogabrielramos28 <joaogabrielramos28@outlook.com>
2021-09-23 18:42:48 -0300

    ✨ create user model

---
+++ back-end/src/models/User.ts
@@ -0,0 +1,21 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+class User {
+    @PrimaryGeneratedColumn('uuid')
+    id:string;
+
+    @Column()
+    email:string;
+
+    @Column()
+    password:string;
+
+    @Column('int')
+    age:Number
+
+    @Column()
+    name:string
+}
+
+export default User;
