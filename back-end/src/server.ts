e2ec226101ecdfd6afea79a4c4485ed69832b21f
joaogabrielramos28 <joaogabrielramos28@outlook.com>
2021-10-21 00:00:43 -0300

    add cors package

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,11 +1,13 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes/index';
 import uploadConfig from './config/upload';
 import AppError from './errors/AppError';
 import './database';
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
