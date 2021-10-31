a206389eed7bb28626401403835694641c632a85
joaogabrielramos28 <joaogabrielramos28@outlook.com>
2021-10-31 14:33:00 -0300

    Login finish

--- front-end/ecommerce/src/context/AuthContext.tsx
+++ front-end/ecommerce/src/context/AuthContext.tsx
@@ -1,14 +1,54 @@
-import React, { createContext, useCallback } from 'react';
+import React, { createContext, useCallback, useState, useContext } from 'react';
+import api from '../services/api';
+import ToastFunction from '../utils/toast';
+
+interface AuthState {
+    token: string;
+    user: object;
+}
 
 const AuthContext: any = createContext({});
 
 const AuthProvider: React.FC = ({ children }) => {
-    const signIn = useCallback(() => {
-        console.log('logado');
+    const [data, setData] = useState<AuthState | null>(() => {
+        const token = localStorage.getItem('@E-commerce:token');
+        const user = localStorage.getItem('@E-commerce:User');
+
+        if (token && user) {
+            return { token, user: JSON.parse(user) };
+        }
+
+        return null;
+    });
+
+    const signIn = useCallback(async ({ email, password }) => {
+        try {
+            const response = await api.post<AuthState>('/sessions', {
+                email: email,
+                password: password,
+            });
+
+            const { token, user } = response.data;
+
+            localStorage.setItem('@E-commerce:token', token);
+            localStorage.setItem('@E-commerce:User', JSON.stringify(user));
+
+            setData({ token, user });
+            ToastFunction('Login successful', 'success');
+        } catch (err) {
+            ToastFunction('Login Error', 'error');
+        }
+    }, []);
+
+    const signOut = useCallback(() => {
+        localStorage.removeItem('@E-commerce:token');
+        localStorage.removeItem('@E-commerce:User');
+
+        setData(null);
     }, []);
 
     return (
-        <AuthContext.Provider value={{ name: 'diego', signIn }}>
+        <AuthContext.Provider value={{ user: data?.user, signIn, signOut }}>
             {children}
         </AuthContext.Provider>
     );
--- front-end/ecommerce/src/pages/Login/index.tsx
+++ front-end/ecommerce/src/pages/Login/index.tsx
@@ -6,37 +6,20 @@ import { Container, Content, Background, Back } from './styles';
 import Button from '../../components/form/button/index';
 
 import { Form } from '@unform/web';
-import api from '../../services/api';
-import ToastFunction from '../../utils/toast';
 import { AuthContext } from '../../context/AuthContext';
-
 interface AuthData {
     email: string;
     password: string;
 }
 
-interface ResponseData {
-    data: DataProps;
-}
-
-interface DataProps {
-    user: object;
-}
-
 const Login = () => {
-    const { signIn } = useContext(AuthContext);
-    const handleSubmit = async (data: AuthData) => {
-        try {
-            const response: ResponseData = await api.post('/sessions', {
-                email: data.email,
-                password: data.password,
-            });
+    const { signIn, user } = useContext(AuthContext);
 
-            signIn;
-            ToastFunction('Login successful', 'success');
-        } catch (err) {
-            ToastFunction('Login Error', 'error');
-        }
+    const handleSubmit = async (data: AuthData) => {
+        signIn({
+            email: data.email,
+            password: data.password,
+        });
     };
     return (
         <>
